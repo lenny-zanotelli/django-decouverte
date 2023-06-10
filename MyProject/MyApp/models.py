0e22a60d6fd936e022ce3f61edbc0efbbb553175
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2023-06-10 16:16:04 +0200

    feat(MyApp): add templates folder with html views for home and article pages

--- MyProject/MyApp/models.py
+++ MyProject/MyApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    article_title = models.CharField(max_length=100)
+    article_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- MyProject/MyApp/views.py
+++ MyProject/MyApp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template("MyApp/index.html")
+    return HttpResponse(template.render(request))
+
+
+def article(request):
+    latest_article = Article.objects.all()
+    template = loader.get_template("MyApp/article.html")
+    context = {
+        "latest_article_list": latest_article,
+    }
+    return HttpResponse(template.render(context, request))
+
